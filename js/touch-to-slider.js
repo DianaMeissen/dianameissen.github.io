44e84c7ff7fcc370cf212a814e93bbfb39b1a55c
Darya Vyshniakova <vishnya054@gmail.com>
2020-03-04 20:36:59 +0200

    bbbbbbbb

--- js/touch-to-slider.js
+++ js/touch-to-slider.js
@@ -2,3 +2,7 @@ function touch() {
     let el = document.getElementById("slider");
     el.addEventListener("touchmove", console.log("touched"), false);
 }
+
+document.getElementById("eyepass__slider").addEventListener("touchmove", alert('touch start'));
+document.addEventListener("drag", alert('draaaaag'));
+document.addEventListener("click", alert('click'));
