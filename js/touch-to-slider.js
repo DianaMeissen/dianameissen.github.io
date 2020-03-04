87dd51b3aa1a6634b7f101ac059c562a5c551eec
Darya Vyshniakova <vishnya054@gmail.com>
2020-03-05 00:08:12 +0200

    klkkmkm

--- js/touch-to-slider.js
+++ js/touch-to-slider.js
@@ -1,11 +1,11 @@
 function touch(event) {
-    let el_id = event.target.id.substring(4, event.target.id.length);
-    let slide = document.getElementById(el_id);
-    slide.removeAttribute("checked");
-    let number = parseInt(el_id.substring(el_id.length - 1, el_id.length));
+    let el_id = event.target.id.substring(4, event.target.id.length), number, new_elem;
+    document.getElementById(el_id).removeAttribute("checked");
+    number = parseInt(el_id.substring(el_id.length - 1, el_id.length));
     number == 5 ? number = 1 : number++;
-    let new_elem = document.getElementById("switch" + number);
+    new_elem = document.getElementById("switch" + number);
     new_elem.setAttribute("checked", true);
+    alert({ new_elem, number, el_id })
 }
 
 document.addEventListener("touchmove", touch);
