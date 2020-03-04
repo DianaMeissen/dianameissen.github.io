25904d40aa388d00c473a48cc8750453e3cc34c4
Darya Vyshniakova <vishnya054@gmail.com>
2020-03-04 20:57:05 +0200

    bnvc ccccc

--- js/touch-to-slider.js
+++ js/touch-to-slider.js
@@ -1,9 +1,7 @@
 function touch(event) {
-    alert(event)
+    alert(event.target)
     //    let el = document.getElementById("slider");
     //   el.addEventListener("touchmove", console.log("touched"), false);
 }
 
 document.addEventListener("touchmove", touch);
-document.addEventListener("drag", touch);
-document.addEventListener("click", touch);
